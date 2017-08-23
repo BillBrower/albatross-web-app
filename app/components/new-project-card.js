bdf8228dc4ba3b74ca5088a9c3e3fe6a7b7d30f7
Kevin Hoffman <kehoffman3@gmail.com>
2017-08-23 13:59:47 -0400

    Generate new category and projects components.

---
+++ app/components/new-project-card.js
@@ -0,0 +1,36 @@
+import Ember from 'ember';
+import { validator, buildValidations } from 'ember-cp-validations';
+import ValidationErrors from '../constants/errors';
+
+const Validations = buildValidations({
+  newProjectName: validator('presence', {
+    presence: true,
+    message: ValidationErrors.presenceError,
+    description: 'Project name'
+  }),
+});
+
+export default Ember.Component.extend(Validations, {
+
+  newProjectName: null,
+  isAddingNewProject: false,
+
+  actions: {
+    saveButtonPressed() {
+      this.set('showErrors', true);
+      const errors = this.get('validations.messages');
+      if (errors.length === 0) {
+        let result = Ember.RSVP.defer();
+        this.get('onSave')(this.get('newProjectName'), result);
+        return result.promise.then(() => {
+          this.set('newProjectName', null);
+        })
+      } else {
+        return Ember.RSVP.reject();
+      }
+    },
+    toggleIsAddingNewProject() {
+      this.toggleProperty('isAddingNewProject')
+    },
+  }
+});
--- app/controllers/app/projects.js
+++ app/controllers/app/projects.js
@@ -2,39 +2,12 @@ import Ember from 'ember';
 
 const { inject: { service } } = Ember;
 
-import { validator, buildValidations } from 'ember-cp-validations';
-import ValidationErrors from '../../constants/errors';
-
-const Validations = buildValidations({
-  newProjectName: validator('presence', {
-    presence: true,
-    message: ValidationErrors.presenceError,
-    description: 'Project name'
-  }),
-});
-
-export default Ember.Controller.extend(Validations,{
+export default Ember.Controller.extend({
 
   currentUser: service('current-user'),
-  newProjectName: null,
-  isAddingNewProject: false,
-
   actions: {
-    saveButtonPressed() {
-      this.set('showErrors', true);
-      const errors = this.get('validations.messages');
-      if (errors.length === 0) {
-        let result = Ember.RSVP.defer();
-        this.send('createProject', this.get('newProjectName'), result);
-        return result.promise.then(() => {
-          this.set('newProjectName', null);
-        })
-      } else {
-        return Ember.RSVP.reject();
+      createNewProject(name, result) {
+        this.send('createProject', name, result)
       }
-    },
-    toggleIsAddingNewProject() {
-      this.toggleProperty('isAddingNewProject')
-    },
   }
 });
