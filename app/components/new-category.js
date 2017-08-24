68c8bf9cc2f6b053770ed53f01a23e56906ce36b
Kevin Hoffman <kehoffman3@gmail.com>
2017-08-24 13:31:10 -0400

    Setup new category component.

--- app/components/new-category.js
+++ app/components/new-category.js
@@ -1,4 +1,37 @@
 import Ember from 'ember';
 
-export default Ember.Component.extend({
+import { validator, buildValidations } from 'ember-cp-validations';
+import ValidationErrors from '../constants/errors';
+
+const Validations = buildValidations({
+  newCategoryName: validator('presence', {
+    presence: true,
+    message: ValidationErrors.presenceError,
+    description: 'Category name'
+  }),
+});
+
+export default Ember.Component.extend(Validations, {
+
+  newCategoryName: null,
+
+  actions: {
+    saveButtonPressed() {
+      this.set('showErrors', true);
+      const errors = this.get('validations.messages');
+      if (errors.length === 0) {
+        let result = Ember.RSVP.defer();
+        this.get('onSave')(this.get('newCategoryName'), result);
+        return result.promise.then(() => {
+          this.set('newCategoryName', null);
+          this.send('toggleIsAddingNewCategory');
+        })
+      } else {
+        return Ember.RSVP.reject();
+      }
+    },
+    toggleIsAddingNewCategory() {
+      this.toggleProperty('isAddingNewCategory')
+    },
+  }
 });
