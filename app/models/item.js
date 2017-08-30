e655e2402910ff2c10f860a4fe3cb424e7d85007
Kevin Hoffman <kehoffman3@gmail.com>
2017-08-30 16:34:01 -0400

    Validate that estimated and actual are greater than 0

--- app/models/item.js
+++ app/models/item.js
@@ -1,19 +1,31 @@
 import DS from 'ember-data';
 import {buildValidations, validator} from "ember-cp-validations";
 import ValidationErrors from "../constants/errors";
+
 const {attr, Model, belongsTo} = DS;
 
 const Validations = buildValidations({
-  actual: validator('presence', {
-    presence: true,
-    message: ValidationErrors.presenceError,
-    description: 'Actual'
-  }),
-  estimated: validator('presence', {
-    presence: true,
-    message: ValidationErrors.presenceError,
-    description: 'Estimated'
-  }),
+  actual: [
+    validator('number', {
+      allowString: true,
+      integer: true,
+      gte: 0,
+    }),
+    validator('presence', {
+      presence: true,
+      message: ValidationErrors.presenceError,
+      description: 'Actual'
+    })],
+  estimated: [
+    validator('number', {
+      allowString: true,
+      integer: true,
+      gt: 0,
+    }), validator('presence', {
+      presence: true,
+      message: ValidationErrors.presenceError,
+      description: 'Estimated'
+    })],
   description: validator('presence', {
     presence: true,
     message: ValidationErrors.presenceError,
@@ -23,9 +35,9 @@ const Validations = buildValidations({
 
 export default Model.extend(Validations, {
 
-  actual: attr('number', {defaultValue:0}),
+  actual: attr('number', {defaultValue: 0}),
   description: attr('string'),
-  estimated: attr('number', {defaultValue:0}),
+  estimated: attr('number', {defaultValue: 0}),
 
   category: belongsTo('category'),
 
