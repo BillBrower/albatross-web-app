feaef1e4e644671081236773605c3cc8147a0809
Kevin Hoffman <kehoffman3@gmail.com>
2017-08-30 16:57:10 -0400

    Protect app routes for authenticated users.

--- app/routes/app.js
+++ app/routes/app.js
@@ -1,6 +1,4 @@
 import Ember from 'ember';
-//import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
+import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 
-//TODO: Use mixin when auth is setup
-export default Ember.Route.extend()
-//export default Ember.Route.extend(AuthenticatedRouteMixin);
+export default Ember.Route.extend(AuthenticatedRouteMixin);
