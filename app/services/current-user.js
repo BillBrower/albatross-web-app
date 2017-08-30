2c1273fd700a80034b8f757bd4ab5df72f899785
Kevin Hoffman <kehoffman3@gmail.com>
2017-08-30 14:59:25 -0400

    Fixing authentication.

--- app/services/current-user.js
+++ app/services/current-user.js
@@ -8,25 +8,31 @@ export default Ember.Service.extend({
   store: service(),
 
   load() {
-    if (this.get('session').isAuthenticated) {
-      return Ember.$.ajax({
-        url: ENV.host + '/api/v1/users/',
-        type: 'GET',
-        headers: {
-          'Accept': 'application/vnd.api+json'
-        },
-        contentType: 'application/vnd.api+json',
-        dataType: 'json',
-      }).then((response) => {
-        const id = response['data']['id'];
-        this.get('store').pushPayload(response);
-        const user = this.get('store').peekRecord('user', id);
-        if (user) {
-          this.set('user', user);
-        }
-      }).catch((error) => {
-        this.get('session').invalidate();
-      })
+    if (this.get('session.isAuthenticated')) {
+      return new Ember.RSVP.Promise((resolve, reject) => {
+      this.get('session').authorize('authorizer:django-token-authorizer', (headerName, headerValue) => {
+        const headers = {};
+        headers[headerName] = headerValue;
+        headers['Accept'] = 'application/vnd.api+json';
+        Ember.$.ajax({
+          url: ENV.host + '/api/v1/users/',
+          type: 'GET',
+          headers: headers,
+          contentType: 'application/vnd.api+json',
+          dataType: 'json',
+        }).then((response) => {
+          const id = response['data']['id'];
+          this.get('store').pushPayload(response);
+          const user = this.get('store').peekRecord('user', id);
+          if (user) {
+            this.set('user', user);
+          }
+          resolve();
+        }).catch(() => {
+          reject();
+        })
+      });
+      });
     } else {
       return Ember.RSVP.reject();
     }
