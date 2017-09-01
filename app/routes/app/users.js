895fbd01f205b5c00b40df568c49a5c468862b4d
Kevin Hoffman <kehoffman3@gmail.com>
2017-09-01 12:40:38 -0400

    Almost have invite users working.

--- app/routes/app/users.js
+++ app/routes/app/users.js
@@ -1,4 +1,17 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+
+  currentUser: Ember.inject.service('current-user'),
+
+  model() {
+    const user = this.get('currentUser.user');
+    return user.get('membership').then((membership) => {
+      return membership.get('team').then((team) => {
+        return this.get('store').findRecord('team', team.get('id'));
+      });
+
+    });
+
+  }
 });
