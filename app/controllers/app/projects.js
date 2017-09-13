dd39f99283ae0634e092382ab18079d86737f591
Austin Price <priceaustinw@gmail.com>
2017-09-13 15:52:21 -0400

    Sprinkled in emoji, edited copy

--- app/controllers/app/projects.js
+++ app/controllers/app/projects.js
@@ -7,6 +7,25 @@ export default Ember.Controller.extend({
   currentUser: service('current-user'),
   sortedProjects: Ember.computed.sort('model', 'sortDefinition'),
   sortDefinition: ['name'],
+  timeOfDay: Ember.computed(function() {
+    const now = new Date();
+    const time = moment(now).hour();
+
+    if (time > 17) {
+      return "evening";
+    } else if (time > 11) {
+      return "afternoon"
+    } else if (time > 3) {
+      return "morning"
+    } else {
+      return "evening"
+    }
+  }),
+
+  init() {
+    this._super(...arguments);
+    console.log(this.get('timeOfDay'));
+  },
 
   actions: {
       createNewProject(name, result) {
