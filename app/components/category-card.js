8415cf3b4ba557b9660801c741fca3b4c764c54a
Austin Price <priceaustinw@gmail.com>
2017-09-15 18:45:24 -0400

    Changed the sorting on category cards

--- app/components/category-card.js
+++ app/components/category-card.js
@@ -2,10 +2,21 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
 
-  isEmpty: Ember.computed('model.actual', 'model.estimated', function() {
-    return this.get('model.actual') === 0 && this.get('model.estimated') === 0;
+  isEmpty: Ember.computed('model.items', 'model.items.@each.isNew', function() {
+    if (this.get('model.items.length') < 1) {
+      console.log("one");
+      return true;
+    } else {
+      if (this.get('model.items.firstObject.isNew')) {
+        console.log("two");
+        return true;
+      } else {
+        console.log("three");
+        return false;
+      }
+    }
   }),
   sortedItems: Ember.computed.sort('model.items', 'sortDefinition'),
-  sortDefinition: ['createdAt'],
+  sortDefinition: ['isNew','description'],
   tagName:''
 });
