09dd1a67da43528b4316e3fdcb9e958c2399769c
Kevin Hoffman <kehoffman3@gmail.com>
2017-10-24 15:04:10 -0400

    When adding an item set focus on description

--- app/components/category-card.js
+++ app/components/category-card.js
@@ -3,9 +3,13 @@ import Ember from 'ember';
 export default Ember.Component.extend({
 
   isEmpty: Ember.computed('model.items', 'model.items.@each.isNew', function() {
+
     if (this.get('model.items.length') < 1) {
       return true;
     } else {
+      Ember.run.schedule("afterRender",this,function() {
+        this.$('input[name=description]:last').focus();
+      });
       if (this.get('model.items.firstObject.isNew')) {
         return true;
       } else {
@@ -16,7 +20,7 @@ export default Ember.Component.extend({
   isDeleting: false,
   sortedItems: Ember.computed.sort('model.items', 'sortDefinition'),
   sortDefinition: ['createdAt'],
-  tagName:'',
+  //tagName:'',
 
   actions: {
     toggleIsDeleting() {
