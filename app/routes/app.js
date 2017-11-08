fb76f9635213ba8128598b157a56acfcb8b7fbe4
Kevin Hoffman <kehoffman3@gmail.com>
2017-11-08 13:00:29 -0500

    If a user is not on trial and has not signed up, route them to the choose plan route.

--- app/routes/app.js
+++ app/routes/app.js
@@ -1,4 +1,18 @@
 import Ember from 'ember';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 
-export default Ember.Route.extend(AuthenticatedRouteMixin);
+const { inject: { service } } = Ember;
+export default Ember.Route.extend(AuthenticatedRouteMixin, {
+
+  currentUser: service('current-user'),
+
+  planObserver: Ember.observer('currentUser.teamPlan', function() {
+    if (!this.get('currentUser.onTrial') && !this.get('currentUser.teamPlan')) {
+      this.transitionTo('choose-plan');
+    }
+  }),
+
+  beforeModel() {
+    this.get('planObserver');
+  }
+});
